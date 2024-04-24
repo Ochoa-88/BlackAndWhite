7e6b84256db39dd131b62dbaff843052b524253b
Ochoa-88 <157509216+Ochoa-88@users.noreply.github.com>
2024-04-24 08:56:52 -0600

    up

--- sources/primaryFrame.js
+++ sources/primaryFrame.js
@@ -2,6 +2,6 @@ var iframe=document.getElementById("primaryFrame");
 function snake() {
 iframe.src = "sources/games/fadingSnake/dist/index.html";
 }
-function tickTackToe() {
+function ticTacToe() {
 iframe.src = "sources/games/ticTacToe/dist/index.html";
 }
