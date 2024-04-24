eaf83e5b026be16316e1a63dbddabd354eeab904
Ochoa-88 <157509216+Ochoa-88@users.noreply.github.com>
2024-04-24 10:00:30 -0600

    up

---
+++ sources/games/pong/README.md
@@ -0,0 +1,4 @@
+# Pong
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/austincondiff/pen/PjJKvd](https://codepen.io/austincondiff/pen/PjJKvd).
+
--- sources/primaryFrame.js
+++ sources/primaryFrame.js
@@ -1,7 +1,22 @@
 var iframe=document.getElementById("primaryFrame");
-function snake() {
+function snake()
+{
 iframe.src = "sources/games/fadingSnake/dist/index.html";
 }
-function ticTacToe() {
+function ticTacToe()
+{
 iframe.src = "sources/games/ticTacToe/dist/index.html";
+}
+function pongG()
+{
+iframe.src = "sources/games/pong/dist/index.html";
+}
+function memoryG()
+{
+iframe.src = "sources/games/dinosaurMemoryGame/dist/index.html";
+}
+
+function refrb()
+{
+iframe.src = "sources/pages/references.html";
 }
